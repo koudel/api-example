47bdf949b74b7f38078c2edf857d73698d70a341
Koudel <koudel.ondra@seznam.cz>
2021-09-13 15:11:19 +0200

    Add examples for product model

--- Product.Service/Product.Service.Main/V1/Models/ProductModel.cs
+++ Product.Service/Product.Service.Main/V1/Models/ProductModel.cs
@@ -14,26 +14,31 @@ namespace Product.Service.Main.V1.Models
         /// <summary>
         /// Unique identificator
         /// </summary>
+        /// <example>4824d9e7-8ca5-4f67-8fa8-ff0542914cb0</example>
         [Required]
         public Guid Id { get; set; }
         /// <summary>
         /// Product name
         /// </summary>
+        /// <example>Awesome shoes</example>
         [Required]
         public string Name { get; set; }
         /// <summary>
         /// Product uri of image
         /// </summary>
+        /// <example>foo://example.com:1234/over/there.png</example>
         [Required]
         public Uri ImgUri { get; set; }
         /// <summary>
         /// Product price
         /// </summary>
+        /// <example>20.51</example>
         [Required]
         public decimal Price { get; set; }
         /// <summary>
         /// Product description
         /// </summary>
+        /// <example>These are best shoes you will ever have.</example>
         public string Description { get; set; }
     }
 }
